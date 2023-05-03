ccd8161c8b200d66c667cb29ff123b40d7fffa3e
Joshua Baskaran <joshbaskaran@gmail.com>
2023-05-03 11:14:16 +0200

    changed config to match repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
 	integrations: [tailwind()],
 	site: 'https://ELIXIR-NO.github.io',
-	base: '/elixir-no',
+	base: '/elixir-no-webpages',
 });
