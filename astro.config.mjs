725e4797a8c151cad4b42fd091ce31b88d073ce5
Joshua Baskaran <joshbaskaran@gmail.com>
2023-05-03 13:13:09 +0200

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	site: 'https://ELIXIR-NO.github.io',
-	base: '/elixir-no-webpages',
+  integrations: [tailwind(), react()],
+  site: 'https://ELIXIR-NO.github.io',
+  base: '/elixir-no-webpages'
 });
