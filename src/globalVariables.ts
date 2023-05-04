63fa16728eb08aceecbc72b55b4f411c061a1f78
Jeanne Cheneby <jeannech@uio.no>
2023-05-04 14:28:59 +0200

    Evething is here

--- src/globalVariables.ts
+++ src/globalVariables.ts
@@ -1,3 +1,4 @@
+// External URL
 export const contactAddress = 'mailto:david.dolan@uib.no'
 export const fkURL = 'https://www.forskningsradet.no'
 export const uioURL = 'https://uio.no'
@@ -5,3 +6,10 @@ export const nmbuURL = 'https://www.nmbu.no'
 export const uitURL = 'https://uit.no'
 export const ntnuURL = 'https://ntnu.no'
 export const uibURL = 'https://uib.no'
+
+// Image path
+export const uibLogoWhite = 'img/logos/UiB_eng_white_center.png'
+export const ntnuLogoWhite = 'img/logos/NTNU_eng_white_center.png'
+export const nmbuLogoWhite = 'img/logos/NMBU_eng_lightgreen_center.png'
+export const uioLogoWhite = 'img/logos/UiO_eng_red_center.png'
+export const frLogoWhite = 'img/logos/FR_eng_white_center.png'
