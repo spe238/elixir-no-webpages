a1306dccb3404d17140ea3941ef3d1938dd25c6f
Joshua Baskaran <joshbaskaran@gmail.com>
2023-05-05 12:40:03 +0200

    put back the base url to repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from '@astrojs/react';
 export default defineConfig({
 	integrations: [tailwind(), react()],
 	site: 'https://ELIXIR-NO.github.io',
-	base: '/',
+	base: '/elixir-no-webpages',
 });
