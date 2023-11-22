aa6e900941c1869ba45692b91b342dbf69899553
danilomartinss <danilolmartins@gmail.com>
2023-11-22 09:37:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/seu-subdiretorio/',
   vite: {
     ssr: {
       noExternal: ["react-icons"],
