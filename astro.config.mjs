8c6a539ff379bd1ee8c9fa81ab86e36963a3ce2e
danilomartinss <danilolmartins@gmail.com>
2023-11-27 16:47:09 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://elixir-no.github.io/',
-  base: '/elixir-no-webpages/',
+  site: 'https://elixir-no.github.io/elixir-no-webpages/',
   vite: {
     ssr: {
       noExternal: ["react-icons"],
